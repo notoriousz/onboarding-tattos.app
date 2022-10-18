3731f9d28999eb5c26fff4b74331db7dfdd227eb
Matheus <mahserafa2016@gmail.com>
2022-10-18 15:42:54 -0300

    initial commit

---
+++ src/main/kotlin/com/api/v1/onboarding/OnboardingTattosApplication.kt
@@ -0,0 +1,11 @@
+package com.api.v1.onboarding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnboardingTattosApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnboardingTattosApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/v1/onboarding/OnboardingTattosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.v1.onboarding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnboardingTattosApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
